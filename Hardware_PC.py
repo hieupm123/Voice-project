7ee868af6716aba3e249c15233c5acc29e0a7783
hieupm123 <73276280+hieupm123@users.noreply.github.com>
2021-07-14 07:59:39 +0700

    hieu

--- Hardware_PC.py
+++ Hardware_PC.py
@@ -2,25 +2,34 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 import math
-# import pya
+
+file = open("out.txt",'r')
+lines = file.readlines()
+M = {}
+cnt = 0
+for a in range(0,len(lines)):
+	lines[a].replace("\n","")
+	M[cnt] = float(lines[a])
+	cnt += 2
+file.close()	
+
 
 class Hardware:
 	def change_volume(self,ok,value = 0):
+		if(value % 2):
+			value += 1
 		devices = AudioUtilities.GetSpeakers()
 		interface = devices.Activate(
 		    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 		volume = cast(interface, POINTER(IAudioEndpointVolume))
 
 		currentVolumeDb = volume.GetMasterVolumeLevel()
-		_VAlUE = [currentVolumeDb + 6,currentVolumeDb -6,0,-65.25] 
+		_VALUE = [currentVolumeDb + 6,currentVolumeDb -6,M[value]] 
 		# 0 : tăng
 		# 1 : giảm
-		# 2 : max
-		# 3 : min
+		# 2 : set
 		try:
-			volume.SetMasterVolumeLevel(_VAlUE[ok], None)
+			volume.SetMasterVolumeLevel(_VALUE[ok], None)
 		except:
 			pass
 		
-
-
--- main.py
+++ main.py
@@ -3,13 +3,25 @@ from Hardware_PC import Hardware
 # test = speech_and_say()
 
 # test.say('Hello')
-# test.say_VN_by_Microsoft("Xin chào bạn Hiếu")
+# test.say_VN_by_Microsoft("")
 # text = test.speech_none_pause()
 # test.say_VN_by_Microsoft(text)
 # test.say_VN_by_Google(text)
 #asdasdasdasdasda
+import pyautogui
+# while(1):
+# 	pyautogui.press("volumeup")
+# 	test = Hardware()
+# 	f = open('out.txt','a')
+# 	f.write(str(test.change_volume(2)))
+# 	f.write("\n")
+# 	f.close()
+# 	if(test.change_volume(2) == 0.0):
+# 		break
+
 test = Hardware()
-test.change_volume(2)
+test.change_volume(2,21)
+
 
 
 # import osascript
