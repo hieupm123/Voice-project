3ed81957453dfcf8008db6f40eb9968a12cb98e9
hieupm123 <73276280+hieupm123@users.noreply.github.com>
2021-07-14 13:40:02 +0700

    hieu

--- Hardware_PC.py
+++ Hardware_PC.py
@@ -3,10 +3,13 @@ from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 import math
 import pyautogui
-import os
+import os,sys
 import time
+from datetime import datetime
+import psutil
+import screen_brightness_control as sbc
 
-# Lấy thông số volume
+# Lấy thông số volume từ file in_volume.txt
 file_volume = open("in_volume.txt",'r')
 lines_volume = file_volume.readlines()
 M_volume = {}
@@ -17,8 +20,15 @@ for a in range(0,len(lines_volume)):
 	index_volume += 2
 file_volume.close()	
 
-#
+# Note: Tất cả các hàm và class có thể gọi = str nên nếu chắc năng nào cần viết nhiều hàm
+# thì chúng ta sẽ gọi hàm = srt
+
 class Hardware:
+	# Hàm cho phép tắt phần mềm = task_manager
+	def killer(self,process_name):
+		os.system('taskkill /f /im ' + process_name)
+
+	# Hàm thay đổi âm lượng
 	def change_volume(self,ok,value = 0):
 		if(value % 2):
 			value += 1
@@ -35,19 +45,83 @@ class Hardware:
 		except:
 			pass
 
-	def unikey(self,ok):
-		# 0 : chuyển đổi chế độ
-		# 1 : Bật bảng mã unikey
-
-		def open_unikey():
+	# Hàm về unikey
+	def unikey(self,func):
+		def open_table_unikey():
 			pyautogui.hotkey('ctrl', 'shift', 'f5')
+
 		def change_unikey():
 			pyautogui.hotkey('ctrl', 'shift')
 			pyautogui.hotkey('alt', 'z')
-		if(ok):
-			open_unikey();
-		else:
-			change_unikey();
+
+		return locals()[func]()
+
+	# Hàm lấy thời gian
+	def get_current_time(self, func):
+		now = datetime.now()
+		def get_time():
+			a = [now.strftime("%H"),now.strftime("%M"),now.strftime("%S")]
+			return a
+		def get_day():
+			a = [now.strftime("%d"),now.strftime("%m"),now.strftime("%Y")]
+			return a
+		return locals()[func]()
+
+	# Hàm cho máy đi ngủ
+	def sleep_computer(self):
+		os.system("Rundll32.exe Powrprof.dll,SetSuspendState Sleep")
+
+	# Hàm tắt máy tính
+	def turn_off_computer(self):
+		os.system("shutdown /s /t 1")
+
+	# Hàm về cpu,ram,battery
+	def get_cpu_ram_battery(self,func):
+
+		def get_ram():
+			return psutil.virtual_memory()[2]
+
+		def get_cpu():
+			return psutil.cpu_percent(4)
+
+		def get_battery():
+			battery = psutil.sensors_battery()
+			plugged = battery.power_plugged
+			percent = str(battery.percent)
+			plugged_EL = "Plugged In" if plugged else "Not Plugged In"
+			plugged_VN = "Đang sạc" if plugged else "Không sạc"
+			return [percent,plugged_EL,plugged_VN]
+
+		return locals()[func]()
+
+	# Hàm tăng giảm độ sáng, và tắt mở tiết kiệm pin
+	def battery_saving_and_screen_brightness(self,func):
+		def battery_saving_on():
+			os.system("powercfg /setdcvalueindex SCHEME_CURRENT SUB_ENERGYSAVER ESBATTTHRESHOLD 100")
+			os.system("powercfg /setactive scheme_current")
+
+		def battery_saving_off():
+			os.system("powercfg /setdcvalueindex SCHEME_CURRENT SUB_ENERGYSAVER ESBATTTHRESHOLD 5")
+			os.system("powercfg /setactive scheme_current")
+
+		def screen_brightness_up(light = '+10'):
+			sbc.set_brightness(light)
+
+		def screen_brightness_down(light = '-10'):
+			sbc.set_brightness(light)
+
+		def screen_brightness_set((str)light):
+			sbc.set_brightness(light)
+
+		locals()[func]()
+
+
+
+
+
+
+
+
 
 
 		
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# from pack_and_run import speech_and_say
+from pack_and_run import speech_and_say
 from Hardware_PC import Hardware
 # while(1):
 # 	pyautogui.press("volumeup")
@@ -9,14 +9,23 @@ from Hardware_PC import Hardware
 # 	f.close()
 # 	if(test.change_volume(2) == 0.0):
 # 		break
-I = Hardware()
-I.unikey(0)
+import psutil
+import os
+import sys
+import wmi
+import screen_brightness_control as sbc
+sbc.set_brightness('+10')
+	
+	
+# from __future__ import print_function
 
-
-
-
-
-
+# def get_cpu_temp():
+#     t = psutil.sensors_temperatures()
+#     for x in ['cpu-thermal', 'cpu_thermal']:
+#         if x in t:
+#             return t[x][0].current
+#     print("Warning: Unable to get CPU temperature!")
+#     return 0 
 
 
 # Hieu sua
