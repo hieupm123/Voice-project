568ba0c78bdc5a885f327019e669bbf456c2b048
hieupm123 <73276280+hieupm123@users.noreply.github.com>
2021-07-14 09:36:41 +0700

    hieu

--- Hardware_PC.py
+++ Hardware_PC.py
@@ -2,18 +2,22 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 import math
+import pyautogui
+import os
+import time
 
-file = open("out.txt",'r')
-lines = file.readlines()
-M = {}
-cnt = 0
-for a in range(0,len(lines)):
-	lines[a].replace("\n","")
-	M[cnt] = float(lines[a])
-	cnt += 2
-file.close()	
+# Lấy thông số volume
+file_volume = open("in_volume.txt",'r')
+lines_volume = file_volume.readlines()
+M_volume = {}
+index_volume = 0
+for a in range(0,len(lines_volume)):
+	lines_volume[a].replace("\n","")
+	M_volume[index_volume] = float(lines_volume[a])
+	index_volume += 2
+file_volume.close()	
 
-
+#
 class Hardware:
 	def change_volume(self,ok,value = 0):
 		if(value % 2):
@@ -24,12 +28,26 @@ class Hardware:
 		volume = cast(interface, POINTER(IAudioEndpointVolume))
 
 		currentVolumeDb = volume.GetMasterVolumeLevel()
-		_VALUE = [currentVolumeDb + 6,currentVolumeDb -6,M[value]] 
-		# 0 : tăng
-		# 1 : giảm
-		# 2 : set
+		_VALUE = [currentVolumeDb + 6,currentVolumeDb -6,M_volume[value]] 
+		# 0 : tăng, 1 : giảm, 2 : set âm thanh 
 		try:
 			volume.SetMasterVolumeLevel(_VALUE[ok], None)
 		except:
 			pass
+
+	def unikey(self,ok):
+		# 0 : chuyển đổi chế độ
+		# 1 : Bật bảng mã unikey
+
+		def open_unikey():
+			pyautogui.hotkey('ctrl', 'shift', 'f5')
+		def change_unikey():
+			pyautogui.hotkey('ctrl', 'shift')
+			pyautogui.hotkey('alt', 'z')
+		if(ok):
+			open_unikey();
+		else:
+			change_unikey();
+
+
 		
--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 # from pack_and_run import speech_and_say
 from Hardware_PC import Hardware
-# test = speech_and_say()
-
-# test.say('Hello')
-# test.say_VN_by_Microsoft("")
-# text = test.speech_none_pause()
-# test.say_VN_by_Microsoft(text)
-# test.say_VN_by_Google(text)
-#asdasdasdasdasda
-import pyautogui
 # while(1):
 # 	pyautogui.press("volumeup")
 # 	test = Hardware()
@@ -18,17 +9,12 @@ import pyautogui
 # 	f.close()
 # 	if(test.change_volume(2) == 0.0):
 # 		break
-
-test = Hardware()
-test.change_volume(2,21)
-
+I = Hardware()
+I.unikey(0)
 
 
-# import osascript
 
-# osascript.osascript("set volume output volume 100")
-# test = Hardware()
-# test.change_volume(1)
+
 
 
 
