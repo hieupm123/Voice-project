be82416ea5a72ffdc098860e7ea850796b5bc008
cutra04 <kimcuong04@gmail.com>
2021-08-07 14:39:50 +0700

    Cutra

--- instagram.py
+++ instagram.py
@@ -7,7 +7,7 @@ class Instagram(object):
     browser = None
 
     def open_ins(self):
-        options = webdriver.ChromeOptions()
+        options =   webdriver.ChromeOptions()
         PATH = r"C:\Users\\AppData\Local\Google\Chrome\User Data\Profile 2"
         PATH1 = PATH[:9] + output_check + PATH[9:]
         options.add_argument("user-data-dir=" + PATH1)
@@ -16,8 +16,6 @@ class Instagram(object):
 
     def close(self):
         self.browser.close()
-
-	# hello
 
     def login(self):
         self.us = self.browser.find_element_by_name("username")
@@ -28,12 +26,20 @@ class Instagram(object):
 class operation_ins(Instagram):
     def search(self,nhap='cutra04'):
         try:
-            self.timkiem = self.browser.find_elements_by_class_name("XTCLo x3qfX")
+            self.timkiem = self.browser.find_element_by_xpath('//div[@aria-label="Tìm kiếm"')
             self.timkiem.send_keys(nhap)
             self.timkiem.send_keys(Keys.RETURN)
         except:
             pass
-# def
+    def turn_compass(self):
+        self.laban = self.browser.find_element_by_xpath('//div[@aria-label="Tìm người"')
+        self.laban.click()
+    def message(self):
+        self.nhantin = self.browser.find_element_by_xpath('//div[@aria-label="Direct"')
+        self.nhantin.click()
+    def home(self):
+        self.trangchu = self.browser.find_element_by_xpath('//div[@aria-label="Instagram"')
+        self.trangchu.click()
 I = Instagram()
 I.open_ins()
 sleep(5)
