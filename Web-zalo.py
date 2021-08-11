3f4bad61b1e2d0c7f7cc3c20125f366fc36fee96
Minh Hieu <hieu@hieu.com>
2021-08-11 21:02:58 +0700

    hieu

--- Web-zalo.py
+++ Web-zalo.py
@@ -3,24 +3,27 @@ from general_web import *
 general = general_web()
 output_check = general.lay_ten_nguoi_dung()
 
+speech = speech_and_say();
+
+def speech_catch_error():
+	text_catch = 'Có gì đó không đúng bạn vui lòng xem lại yêu cầu của mình'
+	speech.say_VN_by_Google(text_catch)
+
 class zalo(object):
 
     trinhduyet = None
+
     def init(self):
         try:
-            options = webdriver.ChromeOptions() 
-            options.add_experimental_option("excludeSwitches", ["enable-logging"])
+            options = webdriver.ChromeOptions()
             PATH = r"C:\Users\\AppData\Local\Google\Chrome\User Data\Profile 2"
             PATH1 = PATH[:9] + output_check + PATH[9:]
             options.add_argument("user-data-dir=" + PATH1)
-            self.trinhduyet = webdriver.Chrome(executable_path=r'D:\Laptrinh\test\chromedriver\chromedriver.exe',chrome_options=options)
-            self.trinhduyet.maximize_window() 
-            self.trinhduyet.implicitly_wait(20)
-            self.trinhduyet.get('https://id.zalo.me/')
-            sleep(3)
+            self.trinhduyet = webdriver.Chrome(executable_path=r'./chrome_driver/chromedriver.exe',chrome_options=options)
+            self.trinhduyet.get("https://chat.zalo.me/?null")
+            time.sleep(3)
         except:
-            pass
-
+            speech_catch_error()
 class tin_nhan_zalo(zalo):
 
     def mo_tin_nhan(self):
@@ -30,7 +33,7 @@ class tin_nhan_zalo(zalo):
             ActionChains(self.trinhduyet).move_to_element(phu_luc[0]).click(phu_luc[0]).perform()
             sleep(1)
         except:
-            return;
+            speech_catch_error();
 
     def search_by_index(self,a=1):
         try:
@@ -40,29 +43,55 @@ class tin_nhan_zalo(zalo):
             ActionChains(self.trinhduyet).move_to_element(chon_nhan_tin[a-1]).click(chon_nhan_tin[a-1]).perform()
             sleep(1)
         except:
-            pass
-        
+            speech_catch_error()
+
+    def chat(self,a=''):
+        try:
+            text = speech.speech_none_pause();
+            if(text != ''):
+                a = text;
+            nhantin = self.trinhduyet.find_element_by_id('input_line_0')
+            nhantin.send_keys(a)
+            sleep(1)
+            nhantin.send_keys(Keys.RETURN)
+        except:
+            speech_catch_error()
+    
+    def click_like(self):
+        try:
+            thaicon = self.trinhduyet.find_element_by_class_name('imgHolder')
+            self.trinhduyet.implicitly_wait(10)
+            ActionChains(self.trinhduyet).move_to_element(thaicon).click(thaicon).perform()
+            sleep(1)
+        except:
+            speech_catch_error()
+
 class tim_kiem_zalo(zalo):
 
     def type_in_search(self, nhap=''):
         try:
+            text = speech.speech_none_pause();
+            if(text != ''):
+                nhap = text;
             tim_kiem = self.trinhduyet.find_element_by_id('contact-search-input')
             self.trinhduyet.implicitly_wait(10)
             ActionChains(self.trinhduyet).move_to_element(tim_kiem).click(tim_kiem).perform()    
             tim_kiem.send_keys(nhap)
             sleep(1)   
         except:
-            pass
+            speech_catch_error()
 
     # a là một số hãy giải quyết số đó 
     def chon_nguoi_muon_tim(self, a=1):
         try:
+            text = speech.speech_none_pause()
+            a = general.get_number(text);
             chon_tin_nhan = self.trinhduyet.find_elements_by_class_name('zl-avatar__photo')
             self.trinhduyet.implicitly_wait(10)
             ActionChains(self.trinhduyet).move_to_element(chon_tin_nhan[a+2]).click(chon_tin_nhan[a+2]).perform()    
             sleep(1)   
         except:
-            pass
+            speech_catch_error()
 
 class danh_ba_zalo(zalo):
 
@@ -73,7 +102,7 @@ class danh_ba_zalo(zalo):
             ActionChains(self.trinhduyet).move_to_element(phu_luc[1]).click(phu_luc[1]).perform()
             sleep(1)
         except:
-            pass
+            speech_catch_error()
 
     def list_friend(self):
         try:
@@ -82,7 +111,7 @@ class danh_ba_zalo(zalo):
             ActionChains(self.trinhduyet).move_to_element(danh_sach[0]).click(danh_sach[0]).perform()
             sleep(1)
         except:
-            pass
+            speech_catch_error()
     def danh_sach_nhom(self):
         try:
             danh_sach = self.trinhduyet.find_elements_by_class_name('fr-conv-item-avatar')
@@ -90,7 +119,7 @@ class danh_ba_zalo(zalo):
             ActionChains(self.trinhduyet).move_to_element(danh_sach[1]).click(danh_sach[1]).perform()
             sleep(1)
         except:
-            pass
+            speech_catch_error()
         
     def truyen_file(self):
         try:
@@ -99,7 +128,7 @@ class danh_ba_zalo(zalo):
             ActionChains(self.trinhduyet).move_to_element(danh_sach[3]).click(danh_sach[3]).perform()
             sleep(1)
         except:
-            pass
+            speech_catch_error()
 
 class to_do(zalo):
 
@@ -110,7 +139,7 @@ class to_do(zalo):
             ActionChains(self.trinhduyet).move_to_element(phu_luc[3]).click(phu_luc[3]).perform()
             sleep(1)
         except:
-            pass
+            speech_catch_error()
 
     def to_do_toi_giao(self):
         try:
@@ -119,7 +148,7 @@ class to_do(zalo):
             ActionChains(self.trinhduyet).move_to_element(cac_muc[0]).click(cac_muc[0]).perform()
             sleep(1)
         except:
-            pass
+            speech_catch_error()
 
     def to_do_can_lam(self):
         try:
@@ -128,7 +157,7 @@ class to_do(zalo):
             ActionChains(self.trinhduyet).move_to_element(cac_muc[1]).click(cac_muc[1]).perform()
             sleep(1)
         except:
-            pass
+            speech_catch_error()
 
     def to_do_theo_gioi(self):
         try:
@@ -137,7 +166,7 @@ class to_do(zalo):
             ActionChains(self.trinhduyet).move_to_element(cac_muc[2]).click(cac_muc[2]).perform()
             sleep(1)
         except:
-            pass
+            speech_catch_error()
  
     def to_do_chua_xong(self):
         try:
@@ -146,7 +175,7 @@ class to_do(zalo):
             ActionChains(self.trinhduyet).move_to_element(cac_muc[0]).click(cac_muc[0]).perform()
             sleep(1)
         except:
-            pass
+            speech_catch_error()
 
     def to_do_da_xong(self):
         try:
@@ -155,16 +184,18 @@ class to_do(zalo):
             ActionChains(self.trinhduyet).move_to_element(cac_muc[1]).click(cac_muc[1]).perform()
             sleep(1)
         except:
-            pass
+            speech_catch_error()
 
     def chon_to_do(self,a=1):
         try:
+            text = speech.speech_none_pause();
+            a = general.get_number(text);
             chon_to_do = self.trinhduyet.find_elements_by_class_name('td-gray-txt-v3')
             self.trinhduyet.implicitly_wait(10)
             ActionChains(self.trinhduyet).move_to_element(chon_to_do[a-1]).click(chon_to_do[a-1]).perform()
             sleep(1)
         except:
-            pass
+            speech_catch_error()
 
 class cai_dat(zalo):
 
@@ -179,10 +210,16 @@ class cai_dat(zalo):
             ActionChains(self.trinhduyet).move_to_element(seting[1]).click(seting[1]).perform()
             sleep(1)
         except:
-            pass
+            speech_catch_error()
 
-    def dong_cai_dat(self):
-        pass
+    def tat_cai_dat(self):
+        try:
+            tat_cai_dat = self.trinhduyet.find_element_by_class_name('fa-close')
+            self.trinhduyet.implicitly_wait(10)
+            ActionChains(self.trinhduyet).move_to_element(tat_cai_dat).click(tat_cai_dat).perform()
+            sleep(1)
+        except:
+            speech_catch_error()
 
 class tai_khoan(zalo):
 
@@ -197,7 +234,7 @@ class tai_khoan(zalo):
             ActionChains(self.trinhduyet).move_to_element(seting[1]).click(seting[1]).perform()
             sleep(1)
         except:
-            pass
+            speech_catch_error()
 
     def tat_tai_khoan(self):
         try:
@@ -206,26 +243,104 @@ class tai_khoan(zalo):
             ActionChains(self.trinhduyet).move_to_element(tat_tai_khoan).click(tat_tai_khoan).perform()
             sleep(1)
         except:
-            pass
+            speech_catch_error()
 
+class check(zalo):
 
+    def check_tin_nhan(self):
+        if(self.trinhduyet == None):
+            try:      
+                options = webdriver.ChromeOptions() 
+                options.add_experimental_option("excludeSwitches", ["enable-logging"])
+                PATH = r"C:\Users\\AppData\Local\Google\Chrome\User Data\Profile 2"
+                PATH1 = PATH[:9] + output_check + PATH[9:]
+                options.add_argument("user-data-dir=" + PATH1)
+                driver_web = webdriver.Chrome(executable_path=r'D.\chromedriver\chromedriver.exe',chrome_options=options)
+                driver_web.implicitly_wait(20)
+                driver_web.get('https://id.zalo.me/')
+                sleep(3)
+                chamdo = driver_web.find_element_by_xpath('//*[@id="main-tab"]/div[2]/div[2]/div[1]/i[2]')
+                text = chamdo.get_attribute("class")
+                driver_web.close()
+                text = text.replace('fa fa-num','Có ')
+                text = text.replace('leftbar-unread unread-red','tin nhắn mới')
+                text = text.replace('5plus','nhiều hơn 5')
+                speech.say_VN_by_Google(text)
+                return
+            except:
+                speech.say_VN_by_Google('Không có tin nhắn mới')
+                return
+        try:
+            chamdo = self.trinhduyet.find_element_by_xpath('//*[@id="main-tab"]/div[2]/div[2]/div[1]/i[2]')
+            text = chamdo.get_attribute("class")
+            text = text.replace('fa fa-num','Có ')
+            text = text.replace('leftbar-unread unread-red','tin nhắn mới')
+            text = text.replace('5plus','nhiều hơn 5')
+            speech.say_VN_by_Google(text)
+            return
+        except:
+            speech.say_VN_by_Google('Không có tin nhắn mới')
+            return
+    
+    def check_thong_bao(self):
+        if(self.driver_web == None):
+            try:  
+                options = webdriver.ChromeOptions() 
+                options.add_experimental_option("excludeSwitches", ["enable-logging"])
+                PATH = r"C:\Users\\AppData\Local\Google\Chrome\User Data\Profile 2"
+                PATH1 = PATH[:9] + output_check + PATH[9:]
+                options.add_argument("user-data-dir=" + PATH1)
+                driver_web = webdriver.Chrome(executable_path=r'D.\chromedriver\chromedriver.exe',chrome_options=options)
+                driver_web.implicitly_wait(20)
+                driver_web.get('https://id.zalo.me/')
+                sleep(3)
 
-=======> cái này vào 1 class tên tùy ý
---> kiếm tra tin nhắn mới lấy cái số rồi xử lí chuỗi nháy cái rồi đọc kết quả cho người dùng
--==> tên hàm check_tin_nhan(self) trả một đoạn text thông  báo
---> viết hàm kiếm tra thông báo mới lấy cái số rồi xử lí chuỗi nháy cái rồi đọc kết quả cho người dùng
--==> check_thong_bao(self) trả một đoạn text thông  báo
---> hàm kiếm tra người nhắn tin gần nhất
--==> lay_ten_nguoi_gan_nhat(self) trả một đoạn text thông báo
-=======> cái này vào 1 class tên tùy ý
---> Thiếu phần nhắn với người kia
--==> chat(self,text = '')
---> Thiếu gọi với người kia
--==> call_nor(self)
---> Thiếu phần thả like cho người kia
--==> click_like(self)
---> gọi video với người kia
--==> call_vid(self)
-========> trong class cai_dat luôn
---> 1 hàm đóng cài đặt
--==> dong_cai_dat(self)
+                chamcam = driver_web.find_element_by_xpath('//*[@id="main-tab"]/div[2]/div[2]/div[3]/i[2]')
+                text = chamcam.get_attribute("class")
+                driver_web.close()
+                text = text.replace('fa fa-num','Có ')
+                text = text.replace('leftbar-unread unread-orange','thông báo mới')
+                speech.say_VN_by_Google(text)
+                return
+            except:
+                speech.say_VN_by_Google('Không có thông báo mới')
+                return
+        try:
+            chamcam = self.trinhduyet.find_element_by_xpath('//*[@id="main-tab"]/div[2]/div[2]/div[3]/i[2]')
+            text = chamcam.get_attribute("class")
+            text = text.replace('fa fa-num','Có ')
+            text = text.replace('leftbar-unread unread-orange','thông báo mới')
+            speech.say_VN_by_Google(text)
+            return
+        except:
+            speech.say_VN_by_Google('Không có thông báo mới')
+            return
+
+    def lay_ten_nguoi_gan_nhat(self):
+        if(self.trinhduyet == None):
+            try:      
+                options = webdriver.ChromeOptions() 
+                PATH = r"C:\Users\\AppData\Local\Google\Chrome\User Data\Profile 2"
+                PATH1 = PATH[:9] + output_check + PATH[9:]
+                options.add_argument("user-data-dir=" + PATH1)
+                driver_web = webdriver.Chrome(executable_path=r'.\chromedriver\chromedriver.exe',chrome_options=options)
+                driver_web.implicitly_wait(20)
+                driver_web.get('https://id.zalo.me/')
+                sleep(1)
+                tennguoigannhat = driver_web.find_element_by_xpath('/html/body/div/div/div[2]/nav/div[2]/div[2]/div[3]/div[1]/div/div[1]/div/div[1]/div/div[3]/div[1]/div[1]/span')
+                a = tennguoigannhat.text 
+                sleep(1)
+                driver_web.close()
+                speech.say_VN_by_Google(a)
+                return
+            except:
+                speech_catch_error()
+                return
+        try:
+            tennguoigannhat = self.trinhduyet.find_element_by_xpath('/html/body/div/div/div[2]/nav/div[2]/div[2]/div[3]/div[1]/div/div[1]/div/div[1]/div/div[3]/div[1]/div[1]/span')
+            a = tennguoigannhat.text 
+            speech.say_VN_by_Google(a)
+            return
+        except:
+            speech_catch_error()
+            return
